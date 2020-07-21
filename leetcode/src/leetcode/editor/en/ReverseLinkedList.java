07451c6c6f8e298665ed92405e137faa05d15d70
lai <lcmdmx@sina.cn>
2020-07-21 22:27:07 +0800

    a

---
+++ leetcode/src/leetcode/editor/en/ReverseLinkedList.java
@@ -0,0 +1,44 @@
+package leetcode.editor.en;
+
+/**
+ * @Author: Lai
+ * @Date: 2020/7/21 20:50
+ * @Version 1.0
+ */
+public class ReverseLinkedList {
+
+//Reverse a singly linked list.
+//
+// Example:
+//
+//
+//Input: 1->2->3->4->5->NULL
+//Output: 5->4->3->2->1->NULL
+//
+//
+// Follow up:
+//
+// A linked list can be reversed either iteratively or recursively. Could you im
+//plement both?
+// Related Topics Linked List
+// 👍 4598 👎 91
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+// Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+}
