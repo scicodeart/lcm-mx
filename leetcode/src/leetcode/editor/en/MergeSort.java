303aed37aef340256e7ba8a9f76694f2acbc5e1a
lai <lcmdmx@sina.cn>
2020-08-19 17:13:41 +0800

    Merge branch 'master' of github.com:scicodeart/lcm-mx
    # Conflicts:
    #	leetcode/.idea/workspace.xml

--- leetcode/src/leetcode/editor/en/MergeSort.java
+++ leetcode/src/leetcode/editor/en/MergeSort.java
@@ -6,4 +6,47 @@ package leetcode.editor.en;
  * @Version 1.0
  */
 public class MergeSort {
+
+    // 归并排序
+    public static int[] merge_sort(int[] arr) {
+        int len = arr.length;
+        int[] result = new int[len];
+        int block, start;
+
+        // 考虑到奇数列数组的情况
+        for(block = 1; block < len*2; block *= 2) {
+            for(start = 0; start <len; start += 2 * block) {
+                int low = start;
+                int mid = (start + block) < len ? (start + block) : len;
+                int high = (start + 2 * block) < len ? (start + 2 * block) : len;
+                //两个块的起始下标及结束下标
+                int start1 = low, end1 = mid;
+                int start2 = mid, end2 = high;
+                //开始对两个block进行归并排序
+                while (start1 < end1 && start2 < end2) {
+                    result[low++] = arr[start1] < arr[start2] ? arr[start1++] : arr[start2++];
+                }
+                while(start1 < end1) {
+                    result[low++] = arr[start1++];
+                }
+                while(start2 < end2) {
+                    result[low++] = arr[start2++];
+                }
+            }
+            int[] temp = arr;
+            arr = result;
+            result = temp;
+        }
+        result = arr;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = {1,4,9,2,5,2,5,3,8};
+        int[] ints = merge_sort(a);
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
+    }
 }
