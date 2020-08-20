ed93b6f511d9d89d24b4f894de996ff364df4179
laichuanmin <lcmdmx@sina.cn>
2020-08-21 01:38:37 +0800

    house robber dynamic program

--- leetcode/src/leetcode/editor/en/HouseRobber.java
+++ leetcode/src/leetcode/editor/en/HouseRobber.java
@@ -50,7 +50,7 @@ public class HouseRobber {
          * 递归式：
          * 选i：OPT(i)max = opt(i-2) + arr[i]
          * 不选i：OPT(i)max = opt(i-1)
-         *
+         * <p>
          * opt[0] = arr[0]
          * opt[i]max = arr[0] || arr[1]
          *
@@ -58,32 +58,60 @@ public class HouseRobber {
          * @return
          */
         public int rob(int[] nums) {
-
-
-
-            return 1;
-        }
-
-        public int rec_rob(int[] arr,int i){
-            if (i == 0){
-                return arr[0];
-            }else if (i == 1){
-                return Math.max(arr[0],arr[1]);
-            }else {
-
-
+            if (nums == null || nums.length == 0){
+                return 0;
             }
-            return 0;
+            return rec_rob(nums, nums.length-1);
         }
 
+        /**
+         * 递归方法(每个i都需要从头算一遍)
+         * @param arr
+         * @param i
+         * @return
+         */
+        public int rec_rob(int[] arr, int i) {
+            if (i == 0) {
+                return arr[0];
+            } else if (i == 1) {
+                return Math.max(arr[0], arr[1]);
+            } else {
+                // 选i
+                int a = rec_rob(arr, i - 2) + arr[i];
+                // 不选i
+                int b = rec_rob(arr, i - 1);
+                return Math.max(a, b);
+            }
+        }
+
+
+        /**
+         * 动态规划（可复用的f(i)）
+         *
+         * @param arr
+         * @return
+         */
+        public int dp_opt(int[] arr){
+            int[] opt = new int[arr.length];
+            opt[0] = arr[0];
+            opt[1]= Math.max(arr[0],arr[1]);
+            for (int j = 2; j < arr.length; j++) {
+                // 选i
+                int a = opt[j-2] + arr[j];
+                // 不选i
+                int b = opt[j-1];
+                opt[j] = Math.max(a,b);
+            }
+            return opt[arr.length - 1];
+        }
 
     }
 
     public static void main(String[] args) {
         HouseRobber.Solution solution = new HouseRobber().new Solution();
-        int[] nums = {1,2,4,1,7,8,3};
-        int[] ints = solution.rob();
-        System.out.println(Arrays.toString(ints));
+        int[] nums = {1, 2, 4, 1, 7, 8, 3};
+        int rob = solution.dp_opt(nums);
+        System.out.println(rob);
 
     }
 //leetcode submit region end(Prohibit modification and deletion)
