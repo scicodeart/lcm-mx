7c4c9935e3d890782df4ac9ae7ba06c5a7d6e3cb
laichuanmin <lcmdmx@sina.cn>
2020-10-30 00:48:06 +0800

    quick sort

---
+++ leetcode/src/leetcode/editor/en/recursion/BFS/WordLadderA.java
@@ -0,0 +1,23 @@
+package leetcode.editor.en.recursion.BFS;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Lai
+ * @version 1.0
+ * @date 2020-10-29 13:04
+ */
+public class WordLadderA {
+    class Solution {
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            // 第 1 步：先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
+            Set<String> wordSet = new HashSet<>(wordList);
+            if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ leetcode/src/leetcode/editor/en/sort/QuickSortA.java
@@ -0,0 +1,29 @@
+package leetcode.editor.en.sort;
+
+/**
+ * @author Lai
+ * @version 1.0
+ * @date 2020-10-30 00:26
+ */
+public class QuickSortA {
+
+    // Java
+    public static void quickSort(int[] array, int begin, int end) {
+        if (end <= begin) return;
+        int pivot = partition(array, begin, end);
+        quickSort(array, begin, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+    static int partition(int[] a, int begin, int end) {
+        // pivot: 标杆位置，counter: 小于pivot的元素的个数
+        int pivot = end, counter = begin;
+        for (int i = begin; i < end; i++) {
+            if (a[i] < a[pivot]) {
+                int temp = a[counter]; a[counter] = a[i]; a[i] = temp;
+                counter++;
+            }
+        }
+        int temp = a[pivot]; a[pivot] = a[counter]; a[counter] = temp;
+        return counter;
+    }
+}
