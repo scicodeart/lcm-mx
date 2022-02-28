12fb07b67cf78efb0e4206d5312cbfc5fbee5cd9
laichuanmin <Hermione.lai@tuya.com>
2022-02-28 10:16:33 +0800

    add：Triangle

--- leetcode/src/leetcode/editor/en/DynamicProgramming/MaximumSubarray.java
+++ leetcode/src/leetcode/editor/en/DynamicProgramming/MaximumSubarray.java
@@ -11,12 +11,9 @@ public class MaximumSubarray {
 
     class Solution {
         public int maxSubArray(int[] nums) {
-            int pre = 0, maxAns = nums[0];
-            for (int x : nums) {
-                pre = Math.max(pre + x, x);
-                maxAns = Math.max(maxAns, pre);
-            }
-            return maxAns;
+
+            //
+            return 1;
         }
     }
 
