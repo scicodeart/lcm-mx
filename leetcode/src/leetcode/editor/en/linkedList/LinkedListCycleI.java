165cdad5d307c550e80b9f7a1f04868a31511e8e
laichuanmin <lcmdmx@sina.cn>
2022-05-27 00:52:37 +0800

    add : merge two linked list

---
+++ leetcode/src/leetcode/editor/en/linkedList/LinkedListCycleI.java
@@ -0,0 +1,58 @@
+package leetcode.editor.en.linkedList;
+
+import java.util.HashSet;
+
+/**
+ * @author Lai
+ * @version 1.0
+ * @date 2022-05-24 22:23
+ */
+public class LinkedListCycleI {
+
+    /**
+     * Definition for singly-linked list.
+     * class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) {
+     * val = x;
+     * next = null;
+     * }
+     * }
+     */
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            HashSet<ListNode> collection = new HashSet<>();
+            while (head != null) {
+                if (collection.contains(head)) {
+                    return true;
+                } else {
+                    collection.add(head);
+                }
+                head = head.next;
+            }
+            return false;
+        }
+    }
+
+    /**
+     * 快慢指针：查看两个指针循环是否会重叠
+     */
+    public class Solution1 {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+    }
+}
---
+++ leetcode/src/leetcode/editor/en/linkedList/ReverseNodesInKGroup.java
@@ -0,0 +1,48 @@
+package leetcode.editor.en.linkedList;
+
+/**
+ * @author Lai
+ * @version 1.0
+ * @date 2022-05-24 23:07
+ */
+public class ReverseNodesInKGroup {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseKGroup(ListNode head, int k) {
+            //判空
+            if (head == null || head.next == null) { return head; }
+            ListNode tail = head;
+            //保证head节点是下一组的头结点
+            for (int i = 0; i < k; i++) {
+                if (tail == null) { return head; }
+                tail = tail.next;
+            }
+
+            ListNode newNode = reverse206(head, tail);
+            //下一轮的开始的地方就是tail
+            head.next = reverseKGroup(tail, k);
+            return newNode;
+        }
+        private ListNode reverse206(ListNode head, ListNode tail) {
+            ListNode pre = null;
+            //为什么这里是！= tail，tail为一组结尾的next节点
+            while (head != tail) {
+                ListNode next = head.next;
+                head.next = pre;
+                pre = head;
+                head = next;
+            }
+            return pre;
+        }
+    }
+}
